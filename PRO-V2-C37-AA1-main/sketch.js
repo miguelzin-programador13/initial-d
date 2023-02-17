312a4329dd5ac2ac3f37d66a747482489ffd73e4
miguelzin-programador13 <107733471+miguelzin-programador13@users.noreply.github.com>
2023-02-17 17:09:12 -0300

    Add files via upload

---
+++ PRO-V2-C37-AA1-main/sketch.js
@@ -0,0 +1,38 @@
+var canvas;
+var backgroundImage, bgImg, car1_img, car2_img, track, car3_img, car4_img;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2;
+var cars = [];
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
+  car3_img=loadImage("./assets/carlão.png")
+  car4_img=loadImage("./assets/a.png")
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
